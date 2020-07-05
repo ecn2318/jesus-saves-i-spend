c9dc7cbc68833476aeb2b0e5df07c673978a6f98
Esther N <estheroids@gmail.com>
2020-07-05 01:39:34 -0400

    heroku port issue

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function (request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function () {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 app.use(logger("dev"));
 
 app.use(compression());
